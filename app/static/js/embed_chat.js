eba41370e826d526d191fce780cb6c9ecd40d0cd
thepriceisright <keithvertrees@gmail.com>
2024-08-06 19:58:06 -0700

    updates to widget

--- app/static/js/embed_chat.js
+++ app/static/js/embed_chat.js
@@ -1,20 +1,28 @@
 (function() {
-    const loadWidget = () => {
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://epona.eqbay.co/chat_widget';
-        iframe.style.position = 'fixed';
-        iframe.style.bottom = '30px';
-        iframe.style.right = '30px';
-        iframe.style.width = '350px';
-        iframe.style.height = '450px';
-        iframe.style.border = 'none';
-        iframe.style.zIndex = '10000';
-        document.body.appendChild(iframe);
+    const openButton = document.createElement('button');
+    openButton.innerText = 'Chat with us';
+    openButton.className = 'open-button';
+    openButton.onclick = () => {
+        document.getElementById('chat-widget-container').style.display = 'block';
+        openButton.style.display = 'none';
     };
 
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-        loadWidget();
-    } else {
-        document.addEventListener('DOMContentLoaded', loadWidget);
-    }
+    document.body.appendChild(openButton);
+
+    const iframe = document.createElement('iframe');
+    iframe.src = 'https://epona.eqbay.co/chat_widget';
+    iframe.style.position = 'fixed';
+    iframe.style.bottom = '0';
+    iframe.style.right = '0';
+    iframe.style.width = '350px';
+    iframe.style.height = '450px';
+    iframe.style.border = 'none';
+    iframe.style.zIndex = '10001';
+    iframe.style.display = 'none';
+
+    iframe.onload = () => {
+        document.getElementById('chat-widget-container').style.display = 'block';
+    };
+
+    document.body.appendChild(iframe);
 })();
